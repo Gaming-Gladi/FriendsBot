ad4b76da661b01ad35478d0bd4e645c27d40976d
GamingGladi <rijurivu456@gmail.com>
2022-01-21 14:27:46 +0530

    Bot online

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,3 +1,4 @@
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
@@ -5,19 +6,18 @@ import net.dv8tion.jda.api.entities.Activity;
 import javax.security.auth.login.LoginException;
 
 public class Bot {
-    public Bot(String token) {
+
+    public static void main(String[] args) {
         try {
-            JDABuilder
-                    .createDefault(token)
+            JDA jda = JDABuilder
+                    .createDefault(Config.getToken())
                     .setActivity(Activity.playing("Valorant"))
                     .setStatus(OnlineStatus.IDLE)
                     .addEventListeners(new Listener())
                     .build();
-        } catch (LoginException e) {
+            jda.awaitReady();
+        } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-    public static void main(String[] args) {
-        new Bot(Config.getToken());
     }
 }
