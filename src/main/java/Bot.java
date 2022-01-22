6814baa9007a6513e27c4b3660fed661c827e2a7
GamingGladi <rijurivu456@gmail.com>
2022-01-22 14:31:09 +0530

    Added Ping Command

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -3,8 +3,6 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 
-import javax.security.auth.login.LoginException;
-
 public class Bot {
 
     public static void main(String[] args) {
--- src/main/java/Listener.java
+++ src/main/java/Listener.java
@@ -1,4 +1,7 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
@@ -11,5 +14,36 @@ public class Listener extends ListenerAdapter {
     @Override
     public void onReady(@NotNull ReadyEvent event) {
         LOGGER.info("{} is ready!", event.getJDA().getSelfUser().getAsTag());
+    }
+
+    @Override
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+        JDA jda = event.getJDA();
+        User author = event.getAuthor();
+        Message message = event.getMessage();
+        String raw = event.getMessage().getContentRaw();
+        MessageChannel channel = event.getChannel();
+
+        if(channel.getType().equals(ChannelType.TEXT)) {
+            Guild guild = event.getGuild();
+            TextChannel textChannel = message.getTextChannel();
+            Member member = event.getMember();
+
+            String name;
+            if(message.isWebhookMessage()) {
+                name = author.getName();
+            } else {
+                assert member != null;
+                name = member.getEffectiveName();
+            }
+        }
+
+        if(raw.equals(Config.getPrefix() + "ping")) {
+            channel.sendMessageFormat("""
+                            Pong!
+                            %sms
+                            """, jda.getGatewayPing())
+                    .queue();
+        }
     }
 }
